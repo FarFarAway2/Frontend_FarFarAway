0ec7903cabf23caacc2b5efcb6fc369fcff6dceb
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-17 20:41:43 +0200

    Created FiltersComponent + Updates
    Created filters component
    Updated navigation bar
    Updated navigation options

--- farfaraway/src/app/app.module.ts
+++ farfaraway/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { NavigationOptionsComponent } from './navigation-options/navigation-options.component';
+import { FiltersComponent } from './filters/filters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    NavigationOptionsComponent
+    NavigationOptionsComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
