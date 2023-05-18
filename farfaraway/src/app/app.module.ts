697461f382fdb775ba0746021bb70ca9500d17b6
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-18 19:24:26 +0200

    Revert "Datepicker testing"
    This reverts commit 3180962138189664f20cd7fcfc360d34a9e4b5a7.

--- farfaraway/src/app/app.module.ts
+++ farfaraway/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,14 +9,6 @@ import { FiltersComponent } from './filters/filters.component';
 import { SearchOfferComponent } from './search-offer/search-offer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatNativeDateModule } from '@angular/material/core';
-import { DatePickerComponent } from './date-picker/date-picker.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MAT_DATE_LOCALE } from '@angular/material/core'
 
 @NgModule({
   declarations: [
@@ -27,22 +18,14 @@ import { MAT_DATE_LOCALE } from '@angular/material/core'
     FiltersComponent,
     SearchOfferComponent,
     LoginComponent,
-    RegisterComponent,
-    DatePickerComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatDatepickerModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatNativeDateModule,
-    ReactiveFormsModule,
-    MatIconModule
+    AppRoutingModule
 
   ],
-  providers: [{provide: MAT_DATE_LOCALE, useValue: 'es-ES'}],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
