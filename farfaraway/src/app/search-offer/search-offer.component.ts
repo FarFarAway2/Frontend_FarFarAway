ae303b3a03827219c25878f8ddbb3839da6b65df
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-05-18 10:03:25 +0200

    Added search offer

---
+++ farfaraway/src/app/search-offer/search-offer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-offer',
+  templateUrl: './search-offer.component.html',
+  styleUrls: ['./search-offer.component.css']
+})
+export class SearchOfferComponent {
+
+}
