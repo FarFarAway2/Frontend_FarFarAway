8fbeb4238c970da9934a6d73604a33a7cd1627af
andalpezzo <andalpezzo@gmail.com>
2023-05-19 18:21:14 +0200

    Juntando index-Offers y filters y tambien navigation-bar con navigation-options

--- farfaraway/src/app/app-routing.module.ts
+++ farfaraway/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OfferPageComponent } from './offer-page/offer-page.component';
 
+import { OfferPageComponent } from './offer-page/offer-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { IndexOffersComponent } from './index-offers/index-offers.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'offer-page',
     component: OfferPageComponent
+  },
+  {
+    path: '',
+    component: IndexOffersComponent,
   }
 ];
 
