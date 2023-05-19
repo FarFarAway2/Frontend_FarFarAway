a7a66707221a943ff7825babb875ce7152a93b89
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-19 12:10:55 +0200

    Updated offerlist & offercard

--- farfaraway/src/app/app-routing.module.ts
+++ farfaraway/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { OfferListComponent } from './offer-list/offer-list.component';
 import { CreateOfferComponent } from './create-offer/create-offer.component';
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'offerlist',
+    component: OfferListComponent,
   },
   {
     path: 'createoffer',
