9a487b0572a97fa023e6af7c7d35afc3c67cf577
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2023-05-21 11:52:39 +0200

    Map with cards-maps

---
+++ farfaraway/src/app/offer-card-map/offer-card-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-card-map',
+  templateUrl: './offer-card-map.component.html',
+  styleUrls: ['./offer-card-map.component.css']
+})
+export class OfferCardMapComponent {
+
+}
