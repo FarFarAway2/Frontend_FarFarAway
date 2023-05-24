d8660c2e48c7b3f7a070c34d8a08e53cf11edd75
Roger Gibert Rovira <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-24 18:13:11 +0200

    Update login.component.ts

--- farfaraway/src/app/login/login.component.ts
+++ farfaraway/src/app/login/login.component.ts
@@ -1,41 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-//import {
-//  SocialAuthService,
-//  GoogleLoginProvider,
-//  SocialUser,
-//} from 'angularx-social-login';
-
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  //loginForm!: FormGroup;
-  //socialUser!: SocialUser;
-  //isLoggedin?: boolean;
-  //constructor(
-  //  private formBuilder: FormBuilder,
-  //  private socialAuthService: SocialAuthService
-  //) {}
-  //ngOnInit() {
-  //  this.loginForm = this.formBuilder.group({
-  //    email: ['', Validators.required],
-  //    password: ['', Validators.required],
-  //  });
-  //  this.socialAuthService.authState.subscribe((user) => {
-  //    this.socialUser = user;
-  //    this.isLoggedin = user != null;
-  //    console.log(this.socialUser);
-  //  });
-  //}
-  //loginWithGoogle(): void {
-  //  this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  //  console.log("Josep sabe");
-  //}
-  //logOut(): void {
-  //  this.socialAuthService.signOut();
-  //}
 }
+
