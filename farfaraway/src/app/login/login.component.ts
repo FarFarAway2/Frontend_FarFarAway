b5419cebe682c7264cb8e339f3632de02a6dbb8a
Roger Gibert Rovira <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-24 18:06:18 +0200

    Update login.component.ts

--- farfaraway/src/app/login/login.component.ts
+++ farfaraway/src/app/login/login.component.ts
@@ -13,32 +13,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-/*
-  loginForm!: FormGroup;
-  socialUser!: SocialUser;
-  isLoggedin?: boolean;
-  constructor(
-    private formBuilder: FormBuilder,
-    private socialAuthService: SocialAuthService
-  ) {}
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-    this.socialAuthService.authState.subscribe((user) => {
-      this.socialUser = user;
-      this.isLoggedin = user != null;
-      console.log(this.socialUser);
-    });
-  }
-  loginWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-    console.log("Josep sabe");
-  }
-  logOut(): void {
-    this.socialAuthService.signOut();
-  }
-*/
-
+  //loginForm!: FormGroup;
+  //socialUser!: SocialUser;
+  //isLoggedin?: boolean;
+  //constructor(
+  //  private formBuilder: FormBuilder,
+  //  private socialAuthService: SocialAuthService
+  //) {}
+  //ngOnInit() {
+  //  this.loginForm = this.formBuilder.group({
+  //    email: ['', Validators.required],
+  //    password: ['', Validators.required],
+  //  });
+  //  this.socialAuthService.authState.subscribe((user) => {
+  //    this.socialUser = user;
+  //    this.isLoggedin = user != null;
+  //    console.log(this.socialUser);
+  //  });
+  //}
+  //loginWithGoogle(): void {
+  //  this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  //  console.log("Josep sabe");
+  //}
+  //logOut(): void {
+  //  this.socialAuthService.signOut();
+  //}
 }
