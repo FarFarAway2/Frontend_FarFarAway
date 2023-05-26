b488e953cf5dc056003357fd017be6d56f784bb7
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-26 09:09:31 +0200

    Get data from login

--- farfaraway/src/app/login/login.component.ts
+++ farfaraway/src/app/login/login.component.ts
@@ -7,5 +7,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  private email = '';
+  private password = '';
 
+  login() {
+    this.email = (<HTMLInputElement>document.getElementById('email')).value;
+    this.password = (<HTMLInputElement>document.getElementById('password')).value;
+    console.log(this.email + ' / ' + this.password);
+  }
 }
