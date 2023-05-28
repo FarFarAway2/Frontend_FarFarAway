5235d211a38048a602677c2d4eb2bbfc11c2d678
AlbertoDPH <114424982+AlbertoDPH@users.noreply.github.com>
2023-05-28 19:03:15 +0200

    update offer-component

--- farfaraway/src/app/index-offers/index-offers.component.ts
+++ farfaraway/src/app/index-offers/index-offers.component.ts
@@ -10,31 +10,23 @@ import { TravelOfferModel } from '../models/travel-offer-model.model';
   styleUrls: ['./index-offers.component.css'],
 })
 export class IndexOffersComponent implements OnInit {
-<<<<<<< Updated upstream
-  zoom = 12;
-  center: google.maps.LatLngLiteral = { lat: 41.3851, lng: 2.1734 };
-  markers: google.maps.LatLngLiteral[] = [];
-=======
-<<<<<<< Updated upstream
-  zoom = 12;
-  center: google.maps.LatLngLiteral = { lat: 41.3851, lng: 2.1734 };
-  markers: google.maps.LatLngLiteral[] = [];
-=======
   @Input() options: google.maps.MapOptions | undefined;
   zoom = 7;
-  center: google.maps.LatLngLiteral = { lat: 42.000, lng: 2.200 };
+  center: google.maps.LatLngLiteral = { lat: 42.0, lng: 2.2 };
+  markers: google.maps.LatLngLiteral[] = [];
   mapOptions: google.maps.MapOptions = {
     streetViewControl: false,
     mapTypeControl: false,
     zoomControl: false,
   };
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
   hotelOffers: HotelOfferModel[] = [];
   travelOffers: HotelOfferModel[] = [];
 
-  constructor(private hotelOfferService: HotelOfferService, private travelOfferService: TravelOfferService) {}
+  constructor(
+    private hotelOfferService: HotelOfferService,
+    private travelOfferService: TravelOfferService
+  ) {}
 
   ngOnInit() {
     this.retrieveHotelOffers();
@@ -56,39 +48,17 @@ export class IndexOffersComponent implements OnInit {
       });
     });
   }
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
 
   retrieveTravelOffers() {
-  this.travelOfferService.getTravelOffers().subscribe((data) => {
+    this.travelOfferService.getTravelOffers().subscribe((data) => {
       this.travelOffers = data;
       console.log(this.travelOffers);
     });
-<<<<<<< Updated upstream
   }
 
   addMarkers(marker: google.maps.LatLngLiteral) {
     this.markers.push(marker);
     console.log(this.markers);
-=======
   }
 
-  addMarkers(marker: google.maps.LatLngLiteral) {
-    this.markers.push(marker);
-    console.log(this.markers);
-=======
-  addMarkers(arg0: { lat: string; lng: string }) {
-    throw new Error('Method not implemented.');
-  }
-
-  updateOffers() {
-    this.name = this.hotelOffers[0].hotel_name;
-    this.price = this.hotelOffers[0].price;
-    this.hotel_description = this.hotelOffers[0].hotel_description;
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-  }
 }
-
