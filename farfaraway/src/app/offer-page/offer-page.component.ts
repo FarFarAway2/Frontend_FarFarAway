6227ae75874b1dc0623c22c177ed0143bb07a51c
andalpezzo <andalpezzo@gmail.com>
2023-05-28 23:26:18 +0200

    Added scroll on map view and added book form with animated button

--- farfaraway/src/app/offer-page/offer-page.component.ts
+++ farfaraway/src/app/offer-page/offer-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HotelOfferService } from '../service/hotel-offer.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -9,10 +9,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class OfferPageComponent implements OnInit {
   hotelOffer: any;
+  @ViewChild('myModal', { static: true }) myModal: ElementRef;
+  @ViewChild('check', { static: true }) check: ElementRef;
 
   constructor(private route: ActivatedRoute, private hotelOfferService: HotelOfferService, private router: Router
     ) {}
 
+  isHoverEnabled = true;
+  i: number = 0;
 
   name = 'Default';
   price = 0;
@@ -34,5 +38,57 @@ export class OfferPageComponent implements OnInit {
       this.start_date = this.hotelOffer.start_date;
       this.expire_date = this.hotelOffer.expire_date;
     });
+  }
+
+  onHover() {
+    if (this.isHoverEnabled) {
+      const progress = document.querySelector(".progress") as HTMLElement;
+      progress.style.backgroundColor = "#770004";
+    }
+  }
+
+  onLeave() {
+    const progress = document.querySelector(".progress") as HTMLElement;
+    progress.style.backgroundColor = "#ac0006";
+  }
+
+  disableHover() {
+    this.isHoverEnabled = false;
+  }
+
+  enableHover() {
+    this.isHoverEnabled = true;
+  }
+
+  //For animate the button like progress bar
+  makeProgress() {
+    this.disableHover();
+
+    if (this.i < 100) {
+      this.i = this.i + 2;
+      const progressBar = document.querySelector(".progress-bar") as HTMLElement;
+      progressBar.style.width = this.i + "%";
+    }
+
+    // Wait for sometime before running this function again
+    setTimeout(() => {
+      this.makeProgress();
+    }, 100);
+  }
+
+  //For close the book form modal
+  closeModal() {
+    const modal = this.myModal.nativeElement;
+    setTimeout(() => {
+      if (modal) {
+        modal.classList.remove('show');
+        modal.style.display = 'none';
+        document.body.classList.remove('modal-open');
+        const modalBackdrop = document.querySelector('.modal-backdrop');
+        if (modalBackdrop) {
+          modalBackdrop.remove();
+        }
+      }
+    }, 6000); // Retraso de 6000 milisegundos (6 segundos)
   }
 }
